c9fbb92d64c83caca12f7763a6e679b3132a4586
Kevin Ruffin <karuffin1992@gmail.com>
2019-05-05 16:03:04 -0400

    Updated operator functions; added more stylings to look more like a calculator

--- calculator/src/components/Calculator.vue
+++ calculator/src/components/Calculator.vue
@@ -1,25 +1,25 @@
 <template>
   <div class="calculator">
-    <div class="display">{{ display }}</div>
-    <div @click="clear" class="button">CE</div>
-    <div @click="sign" class="button">+/-</div>
-    <div @click="percent" class="button">%</div>
-    <div class="button">/</div>
+    <div class="display">{{ current }}</div>
+    <div @click="clear" class="operator button">CE</div>
+    <div @click="sign" class="operator button">+/-</div>
+    <div @click="percent" class="operator button">%</div>
+    <div @click="divide" class="operator button">/</div>
     <div @click="number('7')" class="button">7</div>
     <div @click="number('8')" class="button">8</div>
     <div @click="number('9')" class="button">9</div>
-    <div class="button">X</div>
+    <div @click="multiply" class="operator button">X</div>
     <div @click="number('4')" class="button">4</div>
     <div @click="number('5')" class="button">5</div>
     <div @click="number('6')" class="button">6</div>
-    <div class="button">-</div>
+    <div @click="subtract" class="operator button">-</div>
     <div @click="number('1')" class="button">1</div>
     <div @click="number('2')" class="button">2</div>
     <div @click="number('3')" class="button">3</div>
-    <div class="button">+</div>
+    <div @click="add" class="operator button">+</div>
     <div @click="number('0')" class="zero button">0</div>
     <div @click="decimal" class="button">.</div>
-    <div class="button">=</div>
+    <div @click="equals" class="operator button">=</div>
   </div>
 </template>
 
@@ -28,26 +28,67 @@ export default {
   name: "Calculator",
   data() {
     return {
-      display: ''
+      current: '',
+      previous: null,
+      operator: null,
+      operatorClicked: false
     };
   },
   methods: {
     clear() {
-      this.display = '';
+      // Clear current value
+      this.current = '';
     },
     number(number) {
-      this.display = `${this.display}${number}`;
+      // Need to check if an operator has been clicked already
+      // If so, reset
+      if(this.operatorClicked){
+        this.current = '';
+        this.operatorClicked = false;
+      }
+      this.current = `${this.current}${number}`;
     },
     sign() {
-      this.display = this.display.charAt(0) === '-' ?
-        this.display.slice(1) : `-${this.display}`;
+      // Setting negative/positive status
+      this.current = this.current.charAt(0) === '-' ?
+        this.current.slice(1) : `-${this.current}`;
     },
     percent() {
-      this.display = `${parseFloat(this.display) / 100}`;
+      // Getting percentage of number
+      this.current = `${parseFloat(this.current) / 100}`;
     },
     decimal() {
-      this.display = this.display.indexOf('.') > 0 ?
-        this.display : `${this.display}.`
+      // Only one decimal should exist in a number
+      this.current = this.current.indexOf('.') > 0 ?
+        this.current : `${this.current}.`
+    },
+    setPrevious() {
+      // Used in math operations (below)
+      // Need to set previous number in calc to current number
+      // Then set operatorClicked flag
+      this.previous = this.current;
+      this.operatorClicked = true;
+    },
+    divide() {
+      this.operator = (a,b) => a / b;
+      this.setPrevious();
+    },
+    multiply() {
+      this.operator = (a,b) => a * b;
+      this.setPrevious();
+    },
+    subtract() {
+      this.operator = (a,b) => a - b;
+      this.setPrevious();
+    },
+    add() {
+      this.operator = (a,b) => a + b;
+      this.setPrevious();
+    },
+    equals() {
+      // Uses operator function with values
+      this.current = `${this.operator(parseFloat(this.previous), parseFloat(this.current))}`;
+      this.previous = null;
     }
   }
 };
@@ -55,11 +96,11 @@ export default {
 
 <style scoped>
   .calculator {
-    margin: 0 auto;
+    margin: 100px auto;
     width: 400px;
     display: grid;
     grid-template-columns: repeat(4, 1fr);
-    grid-auto-rows: minmax(50px,auto)
+    grid-auto-rows: minmax(40px,auto);
   }
 
   .zero {
@@ -69,6 +110,8 @@ export default {
   .display{
     grid-column: 1 / 5;
     grid-row: 1;
+    background-color: #4d4e4f;
+    color: white;
   }
 
   .display, .button {
@@ -76,5 +119,10 @@ export default {
     justify-content: center;
     border: 1px solid black;
     font-size: 30px;
+  }
+
+  .operator {
+    background-color: orange;
+    color: white;
   }
 </style>
